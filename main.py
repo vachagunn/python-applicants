ecf4b43fd157b847dd4ee95da7e7fc6e6de48c1c
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-10 13:22:29 +0500

    Вывод значений рейтинга абитуриента.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import pprint
 
+def edit(x):
+    return float(str(x).replace(',', '.'))
+
+
 pr = pprint.PrettyPrinter()
 
 data = pd.read_csv('data_fake.csv', encoding='cp1251', sep=';', decimal=',')
@@ -16,5 +20,7 @@ data = data.drop(ind, axis=1)
 dictionary = {chr(ord('A') + i): data.columns[i] for i in range(data.shape[1])}
 data.columns = dictionary.keys()
 
+data['F'] = data['F'].apply(edit)
+print(data['F'])
 pr.pprint(dictionary)
 print(data.info())
