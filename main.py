1a8c002f4a030b666e108b2d6df476fc78ea04b3
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-10 15:05:58 +0500

    Добавление в датафрейм минимального, среднего и максимального рейтинга.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pprint
 import datetime as dt
 
+pd.set_option('display.max_rows', None)
 pr = pprint.PrettyPrinter()
 
 data = pd.read_csv('data_fake.csv', encoding='cp1251', sep=';', decimal=',')
@@ -23,19 +24,23 @@ data['F'] = data['F'].apply(lambda x: float(str(x).replace(',', '.')))
 # Дата (из string в Date)
 data['Date_dt'] = pd.to_datetime(data['Q'], format="%d.%m.%Y %H:%M")
 df = data['Date_dt'].dt.strftime('%d/%m/%y %H:%M').str.split(" ", expand=True)
-
-print("Столбец даты:\n", data['Date_dt'])
-print("DateFrame:\n", df)
-
 df.columns = ['Date_str', "Hour"]
 data = pd.concat([data, df], axis=1)
 data['Date'] = pd.to_datetime(data['Date_str'], format="%d/%m/%y")
 
-# Пересчитать кол-во личный дел и сгруппировать датафрейм по дате
-df = data.groupby(['Date'])['B'].count()
+df = data.groupby(['Date'])[['B', 'F']].agg({'B': 'count', 'F': ['min', 'mean', 'max']})
 print('--------Группировка---------------')
 print(df)
 print(df.index, type(df.index))
+'''
+-->
+               B      F                   
+           count    min       mean     max
+Date                                      
+2019-01-09     2  73.00  78.770000   84.54
+2019-01-10     3  72.30  77.433333   85.00
+2019-01-11     2  60.10  63.145000   66.19
+'''
 
 # Найдем все личные дела по дате (пример)
 print('----------------------------------')
