6929ba75736483b13f1331bb6f0cc07c8b5e2321
Vachik <55889660+vachagunn@users.noreply.github.com>
2024-01-10 13:18:00 +0500

    Избавился от лишних столбцов и вывел информацию в виде словаря.

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pandas as pd
+import pprint
+
+pr = pprint.PrettyPrinter()
 
 data = pd.read_csv('data_fake.csv', encoding='cp1251', sep=';', decimal=',')
+
+ind = [
+    'Период обучения', 'Срок обучения', 'Курс',
+    'Тип док. об обр.', 'Серия', 'Номер', 'Награды',
+    'Оригинал', 'Коментарии', 'Тех. секретарь', 'Unnamed: 27'
+]
+
+data = data.drop(ind, axis=1)
+
+dictionary = {chr(ord('A') + i): data.columns[i] for i in range(data.shape[1])}
+data.columns = dictionary.keys()
+
+pr.pprint(dictionary)
+print(data.info())
